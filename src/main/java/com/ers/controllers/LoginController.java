bd7f41f702752d751f8a726a41725750a5ae670e
logriffith <logriffith1991@gmail.com>
2020-12-16 13:03:29 -0700

    changes to LoginController

--- src/main/java/com/ers/controllers/LoginController.java
+++ src/main/java/com/ers/controllers/LoginController.java
@@ -1,13 +1,49 @@
 package com.ers.controllers;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ers.models.LoginDTO;
+import com.ers.services.ConfirmLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class LoginController {
-
+	
+	private ObjectMapper objectMapper = new ObjectMapper();
+	private ConfirmLogin confirmLogin = new ConfirmLogin();
+	
 	public void login(HttpServletRequest request, HttpServletResponse response) throws IOException{
+	
+		if(request.getMethod().equals("POST")) {
+			BufferedReader bufferedReader = request.getReader();//returns the body of the request
+			StringBuilder stringBuilder = new StringBuilder();
+			String line = bufferedReader.readLine();//returns the first line of the body of the request
+			while(line != null) {
+				stringBuilder.append(line);
+				line = bufferedReader.readLine();//gets the next line from the body
+			}
+			
+			String body = new String(stringBuilder);//puts the body in String object
+			LoginDTO loginDTO = objectMapper.readValue(body, LoginDTO.class); //takes the JSON data as a string and stores it as a LoginDTO object
+			if(confirmLogin.login(loginDTO.username, loginDTO.password) != null) {
+				HttpSession httpSession = request.getSession();//returns the current session or creates one if it doesn't exist
+				
+				//these setAttributes remain in the server
+				httpSession.setAttribute("user", loginDTO);
+				httpSession.setAttribute("logged in", true);
+				response.setStatus(200);//login was successful
+				response.getWriter().print("Login was Successful");//"Login Successful" is a check for me in Postman, it will not be seen by the client
+			}else {
+				HttpSession httpSession = request.getSession(false);//returns the session or null if one doesn't already exist
+				if(httpSession != null) {
+					httpSession.invalidate();//deletes the session/logs them out
+				}
+			}
+		}
 		
 	}
 	

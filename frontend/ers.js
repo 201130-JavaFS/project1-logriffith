d1ae460dcdb9e5f7760bb5e1d621961039e07413
logriffith <logriffith1991@gmail.com>
2020-12-16 21:13:08 -0700

    front end work

--- frontend/ers.js
+++ frontend/ers.js
@@ -1,12 +1,30 @@
-const url = 'http://localhost:8080/project1';
+const url = 'http://localhost:8080/project1/';
 
 document.getElementById("loginbtn").addEventListener("click",login);
 
 async function login(){
-    let inputtedName = document.getElementById("username").value;
-    let inputtedPass = document.getElementById("password").value;
+    let inputtedUsername = document.getElementById("username").value;
+    let inputtedPassword = document.getElementById("password").value;
+
+    //put the inputted username and password from client into a JS object
     let userInfo = {
-        username : inputtedName,
-        password : inputtedPass
-    };
+        username : inputtedUsername,
+        password : inputtedPassword
+    };                 
+    //Ask Tim about this below. Where does the request come into play here.
+    let loginResponse = await fetch(url + "login", {
+        method : "POST",
+        body : JSON.stringify(userInfo),
+        credentials : "include"
+        //Credentials:include will ensure that the cookie is captured, future fetch requests
+        //will also require this value in order to sent the cookie back.
+    });
+
+    if(loginResponse.status == 200){
+        let user = await loginResponse.json();//get json response and store in JS object
+        document.getElementById("login-row").innerHTML = '';
+        console.log(loginResponse);
+        console.log(user);
+        
+    }
 };

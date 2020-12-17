0dfeaff301993a9e8d94d8da2e6391500b871e7c
logriffith <logriffith1991@gmail.com>
2020-12-16 20:19:58 -0700

    worked on front end

--- frontend/ers.js
+++ frontend/ers.js
@@ -1,1 +1,12 @@
 const url = 'http://localhost:8080/project1';
+
+document.getElementById("loginbtn").addEventListener("click",login);
+
+async function login(){
+    let inputtedName = document.getElementById("username").value;
+    let inputtedPass = document.getElementById("password").value;
+    let userInfo = {
+        username : inputtedName,
+        password : inputtedPass
+    };
+};

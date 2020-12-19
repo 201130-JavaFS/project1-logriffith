220b6ccec89b41aad72c5d134783988d2fd048f9
logriffith <logriffith1991@gmail.com>
2020-12-19 00:13:28 -0700

    worked on my tables

--- frontend/ers.js
+++ frontend/ers.js
@@ -36,13 +36,13 @@ async function login(){
         logoutButton.id = "logoutbtn";
         logoutButton.className = "btn btn-primary";
         logout.appendChild(logoutButton);
+        document.getElementById("logoutbtn").innerText = "Log Out";
 
         let name = document.getElementById("user");
         let identifier = document.createElement("h5");
         identifier.id = "identifyuser";
         name.appendChild(identifier);
 
-        document.getElementById("logoutbtn").innerText = "Log Out";
         let adjustPadding = document.getElementById("login-row");
         adjustPadding.id = "data";
         document.getElementById("identifyuser").innerText = user.role + ": " + user.firstName + " " + user.lastName;
@@ -50,12 +50,65 @@ async function login(){
         //document.getElementById("logoutbtn").addEventListener("click",logout);
 
         let data = document.getElementById("data");
-        
-        if (user.name === "Manager"){
-            let pendingHeading = document.createElement("h4");
-            pendingHeading.id = "pendhead";
-            pendingHeading.className = "heading";
-            data.appendChild(pendingHeading);
+
+        if (user.role === "Manager"){
+            console.log("in Manager Options");
+            let pendHeading = document.createElement("h4");
+            pendHeading.id = "pendhead";
+            pendHeading.className = "heading";
+            data.appendChild(pendHeading);
+            document.getElementById("pendhead"). innerText = "View Pending Reimbursement Requests:";
+            
+            let requests = document.createElement("button");
+            requests.id = "pendingrequests";
+            requests.className = "btn btn-danger";
+            data.appendChild(requests);
+            document.getElementById("pendingrequests").innerText = "Pending Requests";
+
+            let pendTable = document.createElement("table");
+            pendTable.className = "table table-hover table-bordered";
+            pendTable.id = "pendtable";
+
+            let pendLabel = document.createElement("thead");
+            pendLabel.id = "pendlabel";
+
+            let pendRow0 = document.createElement("tr");
+            pendRow0.id = "pendrow0";
+
+            let label1 = document.createElement("th");
+            label1.id = "firstname";
+            pendRow0.appendChild(lable1);
+            document.getElementById("firstname").innerText = "First Name";
+
+            let label2 = document.createElement("th");
+            label2.id = "lastname";
+            pendRow0.appendChild(lable2);
+            document.getElementById("lastname").innerText = "Last Name";
+
+            let label3 = document.createElement("th");
+            label3.className = "amount";
+            pendRow0.appendChild(label3);
+            document.getElementsByClassName("amount").innerText = "Amount";
+
+            let label4 = document.createElement("th");
+            label4.className = "description";
+            pendRow0.appendChild(label4);
+            document.getElementsByClassName("description").innerText = "Description";
+
+            let label5 = document.createElement("th");
+            label5.className = "type";
+            pendRow0.appendChild(label5);
+            document.getElementsByClassName("type").innerText = "Type";
+
+            let label6 = document.createElement("th");
+            label6.id = "status";
+            pendRow0.appendChild(label6);
+            document.getElementsByClassName("status").innerText = "Status";
+
+            pendLabel.appendChild(pendRow0);
+            pendTable.appendChild(pendLabel);
+            data.appendChild(pendTable);
+
 
         }
     }else{
